18367e261d04b1e55049d639929e9c4368d22427
herifanantenana <herifanantenana17@gmail.com>
2024-02-06 21:04:14 +0300

    adding factory and test rspec for base_issue_type

--- spec/factories/base_issue_types.rb
+++ spec/factories/base_issue_types.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :base_issue_type do
-    name { "MyString" }
-    base_location_type { nil }
+    name { Faker::Name.initials }
+    base_location_type { association :base_location_type}
   end
 end
--- spec/models/base_issue_type_spec.rb
+++ spec/models/base_issue_type_spec.rb
@@ -1,5 +1,41 @@
 require 'rails_helper'
 
 RSpec.describe BaseIssueType, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:base_issue_type) { create(:base_issue_type) }
+
+  describe "#default base_issue_type" do
+    it "is valid" do
+      expect(base_issue_type).to be_valid
+    end
+  end
+
+  describe "#name" do
+    it "is not empty" do
+      expect do
+        base_issue_type.name = nil
+        expect(base_issue_type).not_to be_valid
+        base_issue_type.save!
+      end.to raise_error(ActiveRecord::RecordInvalid)
+
+      expect do
+        base_issue_type.name = ""
+        expect(base_issue_type).not_to be_valid
+        base_issue_type.save!
+      end.to raise_error(ActiveRecord::RecordInvalid)
+    end
+  end
+
+  describe "#base_location_type" do
+    it "is not empty" do
+      expect do
+        base_issue_type.base_location_type = nil
+        expect(base_issue_type).not_to be_valid
+        base_issue_type.save!
+      end.to raise_error(ActiveRecord::RecordInvalid)
+    end
+    it "is the correct base_location_type" do
+      expect(base_issue_type.base_location_type.name).not_to eq("Canada")
+      expect(base_issue_type.base_location_type.name).to eq("Paris") # we define this name "Paris" in the factory of the base_location_type
+    end
+  end
 end
