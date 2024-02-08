78a2c105b4bd1597e397cf3d6a9ef932817de364
herifanantenana <herifanantenana17@gmail.com>
2024-02-08 13:35:10 +0300

    rectification errors and optimisation for each attribute error, update for suggestion

--- app/models/concerns/visit_schedule_validatable.rb
+++ app/models/concerns/visit_schedule_validatable.rb
@@ -7,6 +7,7 @@ module VisitScheduleValidatable
     validate :cant_set_invalid_due_at, on: :update, unless: -> { skip_due_at_validation || !checklist&.is_planned }
     validate :cant_set_incompatible_place_and_checklist
     validate :should_have_only_one_of_place_or_residence_or_spot
+    validate :multiple_location_one_already_set
     validate :the_depth_level_of_location_type_in_checklist_should_correspond_one_of_place_or_residence_or_spot_respectively
   end
 
@@ -23,19 +24,33 @@ module VisitScheduleValidatable
   end
 
   def should_have_only_one_of_place_or_residence_or_spot
-    unless [place, residence, spot].one?
-      [:place, :residence, :spot].each do |symbol_location|
-        errors.add(symbol_location, I18n.t("validations.visit_schedule.should_have_only_one_of_place_or_residence_or_spot"))
+    location = {residence: residence, place: place, spot: spot}
+    if location.values.all?(&:present?) || location.values.none?(&:present?)
+      errors.add(:base, I18n.t("validations.visit_schedule.should_have_only_one_of_place_or_residence_or_spot"))
+    end
+  end
+
+  def multiple_location_one_already_set
+    location = {residence: residence, place: place, spot: spot}
+    if location.compact!&.size == 2
+      location.each do |symbol_location, value_location|
+        error_message = "validations.visit_schedule.multiple_location_one_already_set"
+        location.each do |symbol, value|
+          error_message << ".#{symbol.to_s}" if symbol != symbol_location
+        end
+        errors.add(symbol_location, I18n.t(error_message))
       end
     end
   end
 
   def the_depth_level_of_location_type_in_checklist_should_correspond_one_of_place_or_residence_or_spot_respectively
-    existing_of = place || residence || spot
-    if existing_of.nil? || checklist&.location_type&.base_location_type.depth_level.downcase != existing_of.class.name.downcase
-      [:place, :residence, :spot].each do |symbol_location|
-        error_message = I18n.t("validations.visit_schedule.the_depth_level_of_location_type_in_checklist_should_correspond_to.#{symbol_location.to_s}")
-        errors.add(symbol_location, error_message) if existing_of.class.name.downcase == symbol_location.to_s
+    if [residence, place, spot].compact.size == 1
+      existing_of = place || residence || spot
+      if existing_of.nil? || checklist&.location_type&.base_location_type&.depth_level.downcase != existing_of.class.name.downcase
+        [:place, :residence, :spot].each do |symbol_location|
+          error_message = I18n.t("validations.visit_schedule.the_depth_level_of_location_type_in_checklist_should_correspond_to.#{symbol_location.to_s}")
+          errors.add(symbol_location, error_message) if existing_of.class.name.downcase == symbol_location.to_s
+        end
       end
     end
   end
